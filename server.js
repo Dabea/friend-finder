27404bcb4e42896b23a9d32fd8c6d5ac9e3b6974
Abraham <abeis@msn.com>
2018-05-07 11:23:10 -0500

    updated port info

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
 const friendModule = require('./friendModel')
+var fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -22,9 +23,35 @@ app.get('/api/friends', function (req, res) {
 app.post('/api/friends', function (req, res) {
     let newscores = req.body.scores;
     friendModule(newscores).then((data)=> res.send(data))
-    console.log(newscores);
+    updateList(req.body);
 })
 
+app.get('/api/test', function (req, res) {
+    getCompleteList().then(data => res.send(data))
+})
+
+function getCompleteList(){
+    return new Promise ((reslove, reject) =>{
+        fs.readFile('./data/friends.json', 'utf8', function (err, data) {
+            if (err)  throw err;
+            friendData = JSON.parse(data);
+            reslove(friendData);
+          })
+    })
+}
+
+function updateList(friend){
+    const list = getCompleteList();
+    list.then( data =>{
+       data.push(friend);
+       const string = JSON.stringify(data);
+       console.log(string)
+       fs.writeFile( "./data/friends.json", string, "utf8", (err) => {
+           if(err) throw err;
+           console.log("updated")
+       })
+    } )
+}
 
 
  
